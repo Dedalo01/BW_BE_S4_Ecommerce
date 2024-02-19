4637183cf91030024adf19fd8389490b5b4e3d1a
Gaetano <gaem195@gmail.com>
2024-02-19 18:16:30 +0100

    Add shooping cart functionality

---
+++ BW_BE_S4_Ecommerce/Db.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace BW_BE_S4_Ecommerce
+{
+    public static class Db
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["DbEcommerce"].ToString();
+        public static SqlConnection conn = new SqlConnection(connectionString);
+    }
+}
