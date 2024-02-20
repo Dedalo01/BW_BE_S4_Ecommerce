e427cc4b6d739cac6f00f878d47ff6efd6a0d3e9
Gaetano <gaem195@gmail.com>
2024-02-20 10:16:19 +0100

    fix Details.aspx not working

--- BW_BE_S4_Ecommerce/Details.aspx.cs
+++ BW_BE_S4_Ecommerce/Details.aspx.cs
@@ -1,17 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace BW_BE_S4_Ecommerce
 {
     public partial class Details : System.Web.UI.Page
     {
+        private string ProductID;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            {
+                if (Request.QueryString["product"] == null)
+                {
+                    Response.Redirect("Home.aspx");
+                }
+
+                ProductID = Request.QueryString["product"].ToString();
+
+            }
+            try
+            {
+                Db.conn.Open();
+                SqlCommand cmd = new SqlCommand($"SELECT * FROM Prodotto WHERE id={ProductID}", Db.conn);
+                SqlDataReader dataReader = cmd.ExecuteReader();
+                if (dataReader.HasRows)
+                {
+                    dataReader.Read();
+                    txtProduct.InnerText = dataReader["Nome"].ToString();
+                    img.Src = dataReader["Image"].ToString();
+                    txtDescription.InnerText = dataReader["Descrizione"].ToString();
+                    txtPrice.InnerText = $"{dataReader["prezzo"]}€";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+            finally
+            {
+                if (Db.conn.State == System.Data.ConnectionState.Open)
+                {
+                    Db.conn.Close();
+                }
+            }
         }
+
+        protected void btnAddCart_Click(object sender, EventArgs e)
+        {
+            int prodId = int.Parse(ProductID);
+            List<int> products;
+            if (Session["ProductID"] == null)
+            {
+                products = new List<int>();
+            }
+            else
+            {
+                products = (List<int>)Session["ProductID"];
+            }
+
+            products.Add(prodId);
+
+            Session["ProductID"] = products;
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Db.conn.Open();
+                SqlCommand cmd = new SqlCommand($"DELETE FROM Prodotti WHERE id={ProductID}", Db.conn);
+                SqlDataReader dataReader = cmd.ExecuteReader();
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    Response.Write("Prodotto eliminato con successso");
+                    Response.Redirect("Home.aspx");
+                }
+                else
+                {
+
+                    Response.Write("Impossibile eliminare il prodotto.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Response.Write(ex.ToString());
+            }
+            finally
+            {
+                if (Db.conn.State == System.Data.ConnectionState.Open)
+                {
+                    Db.conn.Close();
+                }
+            }
+
+
+
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            Response.Redirect($"Edit.aspx?product={ProductID}");
+        }
+
     }
 }
